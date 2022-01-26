a1ef9229601d7e17dcdfec1c01d137466f73ea47
Evan Cohen <evancohen19@gmail.com>
2022-01-26 00:34:31 -0500

    stacked bars, added title, xaxis labels, changed background color, experimented with bar color

--- starterApp.jl
+++ starterApp.jl
@@ -1,21 +1,36 @@
 using PlotlyJS
-using CSV
-using DataFrames
-using HTTP
-
-# txt = HTTP.get("https://raw.githubusercontent.com/umich-cphds/cov-ind-19-data/master/source_data/package-data/processed/india_daily_barplot.csv")
-# csv = readcsv(txt.body)
+using CSV, DataFrames
 
 data = CSV.read(download("https://raw.githubusercontent.com/umich-cphds/cov-ind-19-data/master/source_data/package-data/processed/india_daily_barplot.csv"), DataFrame)
 
+## to do: hover text, plot colors, move legend below, bar borders
 
-# p = plot(data, x=:date, y =:count, kind=:"bar")
+p = plot(
+        data, 
+        x =:date, 
+        y =:Count, 
+        color =:Type, 
+        text =:text,
+        type =:"bar",
+        Layout(
+            title="Daily number of new COVID-19 cases, fatalities and recovered in India", 
+            barmode="stack", 
+            bargap = 0,
+            plot_bgcolor="#ffffff", 
+            xaxis_tickangle=-45,
+            xaxis_nticks = 8,
+            marker_width = 0, ## not working
+            marker_color=["#ED553B", "#f2c82e", "#138808"] ## not working
+        )
+    )
+# sets bar colors correctly
+# restyle!(p, 1, marker_color="rgba(237, 85, 59, 1)")
+# restyle!(p, 2, marker_color="rgba(242, 200, 46, 1)")
+# restyle!(p, 3, marker_color="rgba(19, 136, 8, 1)")
 
-# colors = c(
-#     "Fatalities" = "#ED553B",
-#     "New Cases"  = "#f2c82e",
-#     "Recovered"  = "#138808"
-# )
+# for testing
+restyle!(p, 1, marker_color="rgba(256, 6, 34, 1)")
+restyle!(p, 2, marker_color="rgba(256, 6, 34, 1)")
+restyle!(p, 3, marker_color="rgba(256, 6, 34, 1)")
 
-p = plot(data, x =:date, y =:Count, color =:Type, text =:text,
-type =:"bar", colors =:colors, bgcolor=:"White")
+p
