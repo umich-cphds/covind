250e1c9d700179c8f52a2506673db6b52b7cb630
evcohen <evcohen@umich.edu>
2022-03-23 12:01:23 -0400

    code clean-up

--- main.js
+++ main.js
@@ -13,22 +13,21 @@ function buildNational()
     makeCountryCompPlot(countryCompData, countryCompDiv)
 }
 
-function buildPlots(id) {
+function buildPlots(locale) {
     var data = "https://raw.githubusercontent.com/umich-cphds/cov-ind-19-data/master/source_data/package-data/processed/new_everything.csv";
-    var locale = id;
 
     var dailyBarplotDiv = document.getElementById("daily_barplot");
     makeDailyBarPlot(data, dailyBarplotDiv, locale);
 
-    var tvrDiv = document.getElementById("time_varying_r");
+    var tvrDiv = document.getElementById("tvr");
     makeTVRPlot(data, tvrDiv, locale);
 
-    var tprDiv = document.getElementById("test_positive_rate");
+    var tprDiv = document.getElementById("tpr");
     makeTPRPlot(data, tprDiv, locale);
 
-    var vaxDiv = document.getElementById("daily_vax_india");
-    makeDailyVaxPlot(data, vaxDiv, id)
+    var vaxDiv = document.getElementById("daily_vax");
+    makeDailyVaxPlot(data, vaxDiv, locale)
 
-    var precVaxDiv = document.getElementById("india_cumul_perc_vax");
-    makePercVaxPlot(data, precVaxDiv, id)
+    var precVaxDiv = document.getElementById("perc_vax");
+    makePercVaxPlot(data, precVaxDiv, locale)
 }
